4171ecc78be13f3a8467a83ba1c84c8dffc1f39d
kateradcliff <kate.radcliff@engineering.digital.dwp.gov.uk>
2024-07-23 17:26:35 +0100

    Add search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,3 +2,26 @@
 // For guidance on how to add JavaScript see:
 // https://prototype-kit.service.gov.uk/docs/adding-css-javascript-and-images
 //
+
+/* $('#search').on('input', function(){
+    var lowercase = $(this).val().toLowerCase();
+    var input = $(this).val();
+    $('.govuk-products .dwp-card').show();  
+    $(".govuk-products .dwp-card:not(:contains("+input+"))").hide(); 
+    console.log(lowercase, input);
+});  */
+
+document.getElementById("search").addEventListener("keyup", function (evt) {
+    [].forEach.call(document.querySelectorAll(".govuk-products .govuk-product"), function (subject) {
+        if (subject.textContent.indexOf(evt.target.value) === -1) {
+            subject.parentElement.classList.add("hide");
+        } else {
+            subject.parentElement.classList.remove("hide");
+        }
+    });
+}, false);
+
+function clearSelection(){      
+   document.getElementById('search').value = '';
+   $('.hide').removeClass('hide');
+}
