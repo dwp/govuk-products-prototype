c64b828b546bd9f4597e1e918d3c5787fb44ddc7
kateradcliff <kate.radcliff@engineering.digital.dwp.gov.uk>
2024-08-09 17:16:28 +0100

    Add filter file to display month in date

---
+++ app/filters.js
@@ -0,0 +1,16 @@
+const govukPrototypeKit = require('govuk-prototype-kit'); 
+const addFilter = govukPrototypeKit.views.addFilter;
+
+let filters = {}; // Define filters as an object 
+filters.toMonth = function(x) {
+    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    if (x > 0 && x <= 12) {
+        return months[x - 1]; // returns the month as per x
+    } else {
+        return 'Invalid month'; // Handle invalid month
+    }
+};
+
+addFilter('toMonth', filters.toMonth);
+module.export = filters;
+
