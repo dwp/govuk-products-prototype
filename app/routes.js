6f2f8b559360f3cc3361453e2fdc671b015e087c
Kate Radcliff <kate.radcliff@engineering.digital.dwp.gov.uk>
2025-01-06 15:20:30 +0000

    Fix routing and saving data
    GitOrigin-RevId: 7cadaf83fc9ae93293e9989bf45aab6390be530c

--- app/routes.js
+++ app/routes.js
@@ -71,3 +71,55 @@ router.post('/mtb5-which-payment-answer', function(request, response) {
         response.redirect("/cost-of-living/mtb5/contact-details-for-low-income-benefits")
     }
 })
+
+// -------------------------------------------------------------------------------------------------------------------------
+// Request your personal information from DWP - v3
+
+router.post('/pir-v3-address', function(request, response) {
+
+    var emailAddress = request.session.data['email-address']
+
+    if (emailAddress == "yes"){
+        response.redirect("/personal-information-request/v3/15")
+    }
+    else {
+        response.redirect("/personal-information-request/v3/11")
+    }
+})
+
+router.post('/pir-v3-requester', function(request, response) {
+
+    var requester1 = request.session.data['pir-v3-requester-1']
+    var requester2 = request.session.data['pir-v3-requester-2']
+
+    if (requester1 == "you" || requester2 == "appointee"){
+        response.redirect("/personal-information-request/v3/35")
+    }
+    else if (requester2 == "solicitor" || requester1 == "other") {
+        response.redirect("/personal-information-request/v3/38")
+    }
+})
+
+router.post('/pir-v3-routing-to-send-info-to-different-address', function(request, response) {
+
+    var emailAddress = request.session.data['pir-v3-email-address']
+
+    if (emailAddress == "yes"){
+        response.redirect("/personal-information-request/v3/4")
+    }
+    else {
+        response.redirect("/personal-information-request/v3/31")
+    }
+})
+
+router.post('/pir-v3-receiving-benefit', function(request, response) {
+
+    var benefit = request.session.data['pir-v3-receiving-benefit']
+
+    if (benefit == "pir-v3-none"){
+        response.redirect("/personal-information-request/v3/38")
+    }
+    else {
+        response.redirect("/personal-information-request/v3/36")
+    }
+})
